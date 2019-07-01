5bd0166b229391869bfca340c40f6aeb86ce85f0
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-01 10:31:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const config = {
 		"title": "Gatsby Gitbook Boilerplate | Hasura",
 		"description": "Documentation built with mdx. Powering learn.hasura.io ",
 		"ogImage": null,
-		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
+		"docsLocation": "https://github.com/RH-sdavey/sean-helper/blob/master/content/",
 		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
 	},
 };
