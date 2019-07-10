5c24983f0d4a669e478cade6934645dc3ca3c742
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-10 19:49:14 +0200

    style changes

--- src/components/heading.js
+++ src/components/heading.js
@@ -2,9 +2,9 @@ import system from "system-components/emotion";
 
 const Heading = system(
   {
-    is: "h2",
-    fontSize: 5,
-    fontWeight: "700",
+    is: "h1",
+    fontSize: 6,
+    fontWeight: "600",
     lineHeight: 1.5,
     mt: 4,
     mb: 3
